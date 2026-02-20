c330ae58d4e36c52f1d7b151ac2e9dee4e2b4aa0
frame-dev <darrylhuber39@gmail.com>
2026-02-20 12:11:02 +0100

    Java Documentation added

---
+++ JavaDocs/tag-search-index.js
@@ -0,0 +1,1 @@
+tagSearchIndex = [{"l":"Konstantenfeldwerte","h":"","k":"18","u":"constant-values.html"},{"l":"Serialisierte Form","h":"","k":"18","u":"serialized-form.html"}];updateSearchResults();
